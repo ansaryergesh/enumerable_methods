d4cbab14f998a73e99cc3f027b17f45d2e1dac31
ansaryergesh <yergeshovansar@gmail.com>
2019-08-15 20:40:48 +0600

    fixed errors by suggestions from rubocop

--- enumerable_method.rb
+++ enumerable_method.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
+# Enumerable module
 module Enumerable
   
+  # my_each method
   def my_each
     help = self 
     i = 0
@@ -12,16 +14,18 @@ module Enumerable
     self
   end
 
+  # my_each_with_index
   def my_each_with_index
     help = self
     i = 0
-    while i < self.length
+    while i < help.length
       yield(help[i], i )
       i += 1
     end
     self
   end
 
+  # my_select
   def my_select 
     array = []
       self.my_each do |x|
@@ -32,6 +36,7 @@ module Enumerable
       array
   end
 
+  # my_all
   def my_all? 
     flag = true
     self.my_each do |x|
@@ -42,6 +47,7 @@ module Enumerable
     flag
   end
 
+  # my_any?
   def my_any?
     flag = false
     self.my_each do |x|
@@ -51,6 +57,7 @@ module Enumerable
       flag  
   end
 
+  # my_none?
   def my_none?
     flag = false
     self.my_each do |x|
@@ -60,6 +67,7 @@ module Enumerable
     flag
   end
 
+  # my_count
   def my_count 
     count = 0
     self.my_each do |x|
@@ -74,6 +82,7 @@ module Enumerable
     count
   end
 
+  # my_map
   def my_map(&block) 
     array = []
     self.my_each do |x|
@@ -82,6 +91,7 @@ module Enumerable
     array
   end
 
+  # my_inject
   def my_inject (*item)
     val = self[0]
     for i in self[1..self.length] do
@@ -92,31 +102,31 @@ module Enumerable
   end
   
   
-  # Test all methods
-  puts "--my_each--"
-  [1,2,3,4].my_each { |num| puts num }
+  # # Test all methods
+  # puts "--my_each--"
+  # [1,2,3,4].my_each { |num| puts num }
   
-  puts "--my_each_with_index--"
-  [1,2,3,4].my_each_with_index { |num,index| puts "#{num} : #{index}"}
+  # puts "--my_each_with_index--"
+  # [1,2,3,4].my_each_with_index { |num,index| puts "#{num} : #{index}"}
   
-  puts "--my_select--"
-  strings = %w(ruby language course)
-  p ss = strings.my_select {|words| words.include?("r") }
+  # puts "--my_select--"
+  # strings = %w(ruby language course)
+  # p ss = strings.my_select {|words| words.include?("r") }
   
-  puts "--my_all--"
-  p ["ab","abc","abcdta"].my_all? { |x| x.length >= 2 }
+  # puts "--my_all--"
+  # p ["ab","abc","abcdta"].my_all? { |x| x.length >= 2 }
   
-  puts "--my_any--"
-  p ["Ariel","Martin","Darwin","Michaleaaa","Art"].my_any? {|a| a.length >= 9}
+  # puts "--my_any--"
+  # p ["Ariel","Martin","Darwin","Michaleaaa","Art"].my_any? {|a| a.length >= 9}
   
-  puts "--my_none--"
-  p ["ab","abc","abcdta"].my_none? { |x| x.length >= 2 }
+  # puts "--my_none--"
+  # p ["ab","abc","abcdta"].my_none? { |x| x.length >= 2 }
   
-  puts "--my_count--"
-  p ["ab","abc","abcdta"].my_count {|x|}
+  # puts "--my_count--"
+  # p ["ab","abc","abcdta"].my_count {|x|}
   
-  puts "--my_map--"
-  [1,2,3,4].my_map { |num| puts num * 2 }
+  # puts "--my_map--"
+  # [1,2,3,4].my_map { |num| puts num * 2 }
   
-  puts "--my_inject--"
-  [2,4,5].my_inject {|a, b| a + b}
+  # puts "--my_inject--"
+  # [2,4,5].my_inject {|a, b| a + b}
